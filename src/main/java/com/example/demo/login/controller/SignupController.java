d29db4b70d61fa47ad4f078da51075579b96e36f
nagi <minami.nagisa.375@gmail.com>
2020-05-08 16:41:12 +0900

    エラーメッセージを個別に出力するように修正、エラーメッセージを英語に切り替え可能にした

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.login.domain.model.GroupOrder;
 import com.example.demo.login.domain.model.SignupForm;
 
 @Controller
@@ -48,7 +49,8 @@ public class SignupController {
 	// ユーザー登録画面のPOST用コントローラー
 	// バリデーション実施
 	@PostMapping("/signup")
-	public String postSignUp(@ModelAttribute @Validated SignupForm form, BindingResult bindingResult, Model model) {
+	public String postSignUp(@ModelAttribute @Validated(GroupOrder.class) SignupForm form, BindingResult bindingResult,
+			Model model) {
 
 		// データバインド失敗の場合
 		// 入力チェックに引っかかった場合、ユーザー登録画面に戻る
--- src/main/java/com/example/demo/login/domain/model/SignupForm.java
+++ src/main/java/com/example/demo/login/domain/model/SignupForm.java
@@ -19,31 +19,31 @@ import lombok.Data;
 public class SignupForm {
 
 	// 必須入力、メールアドレス形式
-	@NotBlank
-	@Email
+	@NotBlank(groups = ValidGroup1.class, message = "{require_check}")
+	@Email(groups = ValidGroup2.class, message = "{email_check}")
 	private String userId; // ユーザーID
 
 	// 必須入力、長さ4から100桁まで、半角英数字のみ
-	@NotBlank
-	@Length(min = 4, max = 100)
-	@Pattern(regexp = "^[a-zA-Z0-9]+$")
+	@NotBlank(groups = ValidGroup1.class, message = "{require_check}")
+	@Length(min = 4, max = 100, groups = ValidGroup2.class, message = "{length_check}")
+	@Pattern(regexp = "^[a-zA-Z0-9]+$", groups = ValidGroup3.class, message = "{pattern_check}")
 	private String password; // パスワード
 
 	// 必須入力
-	@NotBlank
+	@NotBlank(groups = ValidGroup1.class, message = "{require_check}")
 	private String userName; // ユーザー名
 
 	// 必須入力
-	@NotNull
+	@NotNull(groups = ValidGroup1.class, message = "{require_check}")
 	@DateTimeFormat(pattern = "yyyy/MM/dd")
 	private Date birthday;// 誕生日
 
 	// 値が20から100まで
-	@Min(20)
-	@Max(100)
+	@Min(value = 20, groups = ValidGroup2.class, message = "{min_check}")
+	@Max(value = 100, groups = ValidGroup2.class, message = "{max_check}")
 	private int age;// 年齢
 
 	// falseのみ可能
-	@AssertFalse
+	@AssertFalse(groups = ValidGroup2.class, message = "{false_check}")
 	private boolean marriage;// 結婚ステータス
 }
