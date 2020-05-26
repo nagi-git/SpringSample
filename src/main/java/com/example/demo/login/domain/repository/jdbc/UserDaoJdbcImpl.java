1fe12ec6778708e71f162fec02649f1abee095a6
nagi <minami.nagisa.375@gmail.com>
2020-05-27 00:00:11 +0900

    エラーページの作成

--- src/main/java/com/example/demo/login/domain/repository/jdbc/UserDaoJdbcImpl.java
+++ src/main/java/com/example/demo/login/domain/repository/jdbc/UserDaoJdbcImpl.java
@@ -34,7 +34,7 @@ public class UserDaoJdbcImpl implements UserDao {
 		// 1件登録
 		int rowNumber = jdbc.update(
 				"INSERT INTO m_user(user_id," + " password," + " user_name," + " birthday," + " age," + " marriage,"
-						+ " role)" + "VALUES(?,?,?,?,?,?,?)",
+						+ " role)" + " VALUES(?,?,?,?,?,?,?)",
 				user.getUserId(), user.getPassword(), user.getUserName(), user.getBirthday(), user.getAge(),
 				user.isMarriage(), user.getRole());
 
